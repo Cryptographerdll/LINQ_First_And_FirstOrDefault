8a95ce269e557a5ccc4cb7522eedcb5e47855204
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-15 17:15:15 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,14 +10,18 @@ namespace LINQ_First_And_FirstOrDefault
         static void Main(string[] args)
         {
             string[] Names = { "IronMan", "SuperMan", "BatMan", "SpiderMan", "Bora" };
+            
+         #comment =>   var FirstItem = Names.First(); // IronMan .
+            
+         #comment =>   var FirstItem = Names.FirstOrDefault(a => a.StartsWith("B")); // BatMan 
 
-            var FirstItem = Names.FirstOrDefault(a => a.StartsWith("Z")); // BatMan 
+            var FirstItem = Names.FirstOrDefault(a => a.StartsWith("Z")); // Exception Error 
 
             // In case of writing Unexistant instruction Exception will happned
 
-            // For example : Names.First(a => a.StartsWith("Z"));
+            #comment => For example : Names.First(a => a.StartsWith("Z"));
             // Here ther is no element in list start with Z Soo Programm Error for Avoide this problem 
-            // We use FirstOrDefault Method . 
+            #comment =>  We use FirstOrDefault(); Method . 
 
             Console.Write(FirstItem == null);
         }
