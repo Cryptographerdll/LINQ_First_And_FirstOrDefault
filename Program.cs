26d0c055b0fbfef1b3a25b83c3304a94149787cf
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-15 17:13:10 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+
+namespace LINQ_First_And_FirstOrDefault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Names = { "IronMan", "SuperMan", "BatMan", "SpiderMan", "Bora" };
+
+            var FirstItem = Names.FirstOrDefault(a => a.StartsWith("Z")); // BatMan 
+
+            // In case of writing Unexistant instruction Exception will happned
+
+            // For example : Names.First(a => a.StartsWith("Z"));
+            // Here ther is no element in list start with Z Soo Programm Error for Avoide this problem 
+            // We use FirstOrDefault Method . 
+
+            Console.Write(FirstItem == null);
+        }
+    }
+}
